ed42a7126992aa1546d1018ab986c99725672312
Matt Reyes and Alan Denison <a@a.com>
2016-06-30 09:15:22 -0700

    finish todo list with categories

--- Objects/Task.cs
+++ Objects/Task.cs
@@ -5,14 +5,11 @@ namespace ToDoList
   public class Task
   {
     private string _description;
-    private int _id;
-    private static List<Task> _instances = new List<Task> {};
 
     public Task(string description)
     {
       _description = description;
-      _instances.Add(this);
-      _id = _instances.Count;
+
     }
 
     public string GetDescription()
@@ -23,22 +20,6 @@ namespace ToDoList
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
-    }
-    public int GetId()
-    {
-      return _id;
-    }
-    public static List<Task> GetAll()
-    {
-      return _instances;
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-    public static Task Find(int searchId)
-    {
-      return _instances[searchId-1];
     }
   }
 }
